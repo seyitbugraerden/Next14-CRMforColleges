cec1867315ffabda4cc2a5ff72d65a481bb23fa8
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-18 19:02:51 +0300

    Fixes

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,4 +1,3 @@
-import Chart from "@/components/Charts/page";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
@@ -10,11 +9,7 @@ export const metadata: Metadata = {
 };
 
 const BasicChartPage: React.FC = () => {
-  return (
-    <DefaultLayout>
-      <Chart />
-    </DefaultLayout>
-  );
+  return <DefaultLayout></DefaultLayout>;
 };
 
 export default BasicChartPage;
