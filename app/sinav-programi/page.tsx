41dd8daf75dae50d63f2d652f828c02df6a582a4
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-19 18:18:26 +0300

    new route

---
+++ app/sinav-programi/page.tsx
@@ -0,0 +1,12 @@
+import Calendar from "@/components/Calender";
+import React from "react";
+
+function SınavProgramı() {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+}
+
+export default SınavProgramı;
